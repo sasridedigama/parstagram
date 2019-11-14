821b5f38eb78ca5bbd7d290b18ea766db707a315
sdedigam <sdedigam@ucsd.edu>
2019-11-13 17:31:00 -0800

    parstagram

---
+++ parstagram/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  parstagram
+//
+//  Created by Sasri Dedigama on 11/13/19.
+//  Copyright © 2019 Sasri Dedigama. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
